ee9ba4acda8c418992d7de0cca41bd6652617f8a
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-18 15:11:34 +0200

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./lib/supabaseClient";
 import CarsList from "./components/CarsList";
+import Header from "./components/Header";
 
 function App() {
   const [cars, setCars] = useState([]);
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       {cars.length > 0 ? <CarsList cars={cars} /> : <p>Loading...</p>}
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div>
+      <a>Home</a>
+      <a>Cars for sale</a>
+      <a>Sell your car</a>
+      <a>Menu</a>
+    </div>
+  );
+}
+
+export default Header;
