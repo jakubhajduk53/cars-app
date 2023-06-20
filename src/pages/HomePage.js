4be482232eb3a9495bedbba5332cd762806ffbae
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-20 21:55:27 +0200

    feat: update HomePage component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,13 @@
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex justify-center">
+      <Link to="/cars-for-sale">
+        <Button value="Get Started" />
+      </Link>
+    </div>
+  );
 }
 
 export default HomePage;
