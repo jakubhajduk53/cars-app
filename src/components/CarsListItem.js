99391af944b8c87d973bc840076c4b4e31708a56
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-20 17:32:13 +0200

    style: add styling for CarList

--- src/components/CarsListItem.js
+++ src/components/CarsListItem.js
@@ -1,11 +1,21 @@
+import Button from "./Button";
 function CarsListItem(props) {
   return (
-    <div>
-      <img src={props.img} alt={props.name} />
-      <p>
-        {props.name} - ${props.price} - {props.yearOfProduction} -
-        {props.location}
-      </p>
+    <div className="flex items-center px-4 py-2">
+      <img
+        src={props.img}
+        alt={props.name}
+        className="w-16 h-16 mr-4 rounded-full"
+      />
+      <div>
+        <p className="text-xl font-semibold">{props.name}</p>
+        <p className="text-gray-600">Cost: ${props.price}</p>
+        <p className="text-gray-600">
+          Year of production: {props.yearOfProduction}
+        </p>
+        <p className="text-gray-600">Location: {props.location}</p>
+        <Button value="Buy now" />
+      </div>
     </div>
   );
 }
