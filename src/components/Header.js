0f6493c3764e52266077f3128a348f11368c40b1
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-20 16:45:52 +0200

    style: change style for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
 function Header() {
   return (
-    <div>
-      <a href="">Home</a>
-      <a href="">Cars for sale</a>
-      <a href="">Sell your car</a>
-      <a href="">Menu</a>
+    <div className="flex justify-center bg-neutral-100">
+      <a
+        href=""
+        className="px-8 py-4 text-lg text-black-500 hover:text-blue-700"
+      >
+        Home
+      </a>
+      <a
+        href=""
+        className="px-8 py-4 text-lg text-black-500 hover:text-blue-700"
+      >
+        Cars for sale
+      </a>
+      <a
+        href=""
+        className="px-8 py-4 text-lg text-black-500 hover:text-blue-700"
+      >
+        Sell your car
+      </a>
+      <a
+        href=""
+        className="px-8 py-4 text-lg text-black-500 hover:text-blue-700"
+      >
+        Menu
+      </a>
     </div>
   );
 }
