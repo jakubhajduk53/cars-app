1823b7c2e23d348171d05d8558c7f54c3e5d3c9c
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-20 17:19:34 +0200

    style: add styles for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 function Footer() {
-  return <div>Jakub Hajduk 2023</div>;
+  return (
+    <div className="flex justify-center bg-neutral-100">
+      <p className=" px-8 py-4 text-sm ">Jakub Hajduk 2023</p>
+    </div>
+  );
 }
 
 export default Footer;
