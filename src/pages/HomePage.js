08cae1b0a16eb35059326ebd793eb5ccbdf4dda9
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-21 17:46:47 +0200

    style: add styles for HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,9 +2,18 @@ import Button from "../components/Button";
 import { Link } from "react-router-dom";
 function HomePage() {
   return (
-    <div className="flex justify-center">
+    <div
+      className="flex min-h-screen justify-center items-center bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage:
+          "url(https://cdn.pixabay.com/photo/2021/12/18/15/51/car-6879134_1280.jpg)",
+      }}
+    >
       <Link to="/cars-for-sale">
-        <Button value="Get Started" />
+        <Button
+          value="Get Started"
+          className="relative overflow-hidden bg-transparent hover:bg-opacity-70 text-white uppercase border border-gray"
+        />
       </Link>
     </div>
   );
