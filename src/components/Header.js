2e919af640a495d856f4b16f570f93655f757b0d
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-21 17:11:12 +0200

    fix: update Link styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,22 +9,33 @@ import {
 function Header() {
   return (
     <div className="flex justify-center bg-neutral-100 sticky top-0 ">
-      <div className="px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1">
-        <AiFillHome />
-        <Link to={"/"}>Home</Link>
-      </div>
-      <div className="px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1">
-        <AiFillCar />
-        <Link to={"cars-for-sale"}>Cars for sale</Link>
-      </div>
-      <div className="px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1">
-        <AiOutlineCar />
-        <Link to={"sell-your-car"}>Sell your car</Link>
-      </div>
-      <div className="px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1">
-        <AiOutlineMenu />
-        <Link to={"menu"}>Menu</Link>
-      </div>
+      <Link to={"/"}>
+        <div className="px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1">
+          <AiFillHome />
+          Home
+        </div>
+      </Link>
+
+      <Link to={"cars-for-sale"}>
+        <div className="px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1">
+          <AiFillCar />
+          Cars for sale
+        </div>
+      </Link>
+
+      <Link to={"sell-your-car"}>
+        <div className="px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1">
+          <AiOutlineCar />
+          Sell your car
+        </div>
+      </Link>
+
+      <Link to={"menu"}>
+        <div className="px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1">
+          <AiOutlineMenu />
+          Menu
+        </div>
+      </Link>
     </div>
   );
 }
