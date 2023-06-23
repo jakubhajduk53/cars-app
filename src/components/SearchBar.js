5c13095732942a5f174189cb578b711557254ff0
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-23 19:04:07 +0200

    fix: update SearchBar to redux compatible

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,33 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Button from "./Button";
+import { useDispatch } from "react-redux";
+import { changeSearchTerm } from "../store";
 
-function SearchBar(props) {
-  const [value, setValue] = useState("");
+function SearchBar() {
+  const dispatch = useDispatch();
+  const [localSearchTerm, setLocalSearchTerm] = useState("");
 
-  const handleClick = (event) => {
+  const handleChange = (event) => {
+    setLocalSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    props.handleSearch(value);
+    dispatch(changeSearchTerm(localSearchTerm));
   };
 
   return (
-    <form className="flex items-center gap-8 m-5 w-3/4 mx-auto">
+    <form
+      className="flex items-center gap-8 m-5 w-3/4 mx-auto"
+      onSubmit={handleSubmit}
+    >
       <input
         className="flex-grow px-4 py-2 border border-gray-300 rounded-md"
         type="text"
-        value={value}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
+        value={localSearchTerm}
+        onChange={handleChange}
       />
-      <Button onClick={handleClick} value="Search" />
+      <Button value="Search" />
     </form>
   );
 }
