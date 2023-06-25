891c94701f78857a2fee31617735461ff01259dc
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-25 19:21:37 +0200

    feat: add pagination

--- src/components/CarsList.js
+++ src/components/CarsList.js
@@ -1,23 +1,60 @@
 import CarsListItem from "./CarsListItem";
+import Button from "./Button";
+import { useState } from "react";
 
 function CarsList(props) {
   const cars = props.cars;
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const itemsToShow = cars.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(cars.length / itemsPerPage);
+
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      {cars.map((car) => {
-        return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {itemsToShow.map((car) => (
           <CarsListItem
             key={car.id}
-            id={car.id}
             img={car.image_url}
             name={car.name}
             price={car.price}
             yearOfProduction={car.year_of_production}
             location={car.location}
           />
-        );
-      })}
+        ))}
+      </div>
+      <div className="flex justify-center mt-4">
+        <Button
+          value="<"
+          onClick={goToPreviousPage}
+          disabled={currentPage === 1}
+        />
+        <p className="mx-2">
+          Page {currentPage} of {totalPages}
+        </p>
+        <Button
+          value=">"
+          onClick={goToNextPage}
+          disabled={currentPage === totalPages}
+        />
+      </div>
     </div>
   );
 }
