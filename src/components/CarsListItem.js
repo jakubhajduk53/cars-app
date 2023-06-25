c12816fa77a1e62ac14134234d4b7671c55e917b
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-25 18:58:06 +0200

    style: add border to car offers

--- src/components/CarsListItem.js
+++ src/components/CarsListItem.js
@@ -1,8 +1,8 @@
 import Button from "./Button";
 function CarsListItem(props) {
   return (
-    <div className="flex items-center px-4 py-2 w-full h-full justify-center">
-      <img src={props.img} alt={props.name} className="w-24 h-24 mr-4 " />
+    <div className="flex items-center px-4 py-2 w-full h-full justify-center border hover:border-black">
+      <img src={props.img} alt={props.name} className="w-32 h-32 mr-4 " />
       <div>
         <p className="text-xl font-semibold">{props.name}</p>
         <p className="text-gray-600">Cost: ${props.price}</p>
