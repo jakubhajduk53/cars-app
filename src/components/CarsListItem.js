791c0973cc40b44b4c0c794007671420ba75c8cf
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-25 19:08:22 +0200

    style: change layout of entire CarListItem

--- src/components/CarsListItem.js
+++ src/components/CarsListItem.js
@@ -1,15 +1,20 @@
 import Button from "./Button";
+
 function CarsListItem(props) {
   return (
-    <div className="flex items-center px-4 py-2 w-full h-full justify-center border hover:border-black">
-      <img src={props.img} alt={props.name} className="w-32 h-32 mr-4 " />
+    <div className="flex items-center px-4 py-2 w-full h-full justify-center border border-gray-300 hover:border-black">
+      <img
+        src={props.img}
+        alt={props.name}
+        className="w-48 h-48 mr-4 border border-gray-300"
+      />
       <div>
-        <p className="text-xl font-semibold">{props.name}</p>
-        <p className="text-gray-600">Cost: ${props.price}</p>
-        <p className="text-gray-600">
+        <p className="text-xl font-semibold mb-2">{props.name}</p>
+        <p className="text-gray-600 mb-2">Cost: ${props.price}</p>
+        <p className="text-gray-600 mb-2">
           Year of production: {props.yearOfProduction}
         </p>
-        <p className="text-gray-600">Location: {props.location}</p>
+        <p className="text-gray-600 mb-4">Location: {props.location}</p>
         <Button value="Check Availability" />
       </div>
     </div>
