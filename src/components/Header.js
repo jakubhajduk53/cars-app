59c21af0c6a2b543e92fc6a7d6809b8d19887820
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-26 16:50:04 +0200

    style: add activeLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   AiFillCar,
   AiFillHome,
@@ -7,16 +8,42 @@ import {
 import RouterLink from "./RouterLink";
 
 function Header() {
+  const [activeLink, setActiveLink] = useState(null);
+
+  const handleLinkClick = (name) => {
+    setActiveLink(name);
+  };
+
   return (
     <div className="flex justify-center bg-neutral-100 sticky top-0">
-      <RouterLink to="/" icon={AiFillHome} name="Home" />
-      <RouterLink to="/cars-for-sale" icon={AiFillCar} name="Cars for sale" />
+      <RouterLink
+        to="/"
+        icon={AiFillHome}
+        name="Home"
+        activeLink={activeLink}
+        onClick={handleLinkClick}
+      />
+      <RouterLink
+        to="/cars-for-sale"
+        icon={AiFillCar}
+        name="Cars for sale"
+        activeLink={activeLink}
+        onClick={handleLinkClick}
+      />
       <RouterLink
         to="/sell-your-car"
         icon={AiOutlineCar}
         name="Sell your car"
+        activeLink={activeLink}
+        onClick={handleLinkClick}
       />
-      <RouterLink to="/menu" icon={AiOutlineMenu} name="Menu" />
+      <RouterLink
+        to="/menu"
+        icon={AiOutlineMenu}
+        name="Menu"
+        activeLink={activeLink}
+        onClick={handleLinkClick}
+      />
     </div>
   );
 }
--- src/components/RouterLink.js
+++ src/components/RouterLink.js
@@ -2,13 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 
-const RouterLink = ({ to, icon: Icon, name }) => {
+const RouterLink = ({ to, icon: Icon, name, activeLink, onClick }) => {
   const linkStyles = classNames(
-    "px-8 py-4 text-lg text-black-500 hover:text-blue-700 flex items-center gap-1 transition duration-150"
+    "px-8 py-4 text-lg",
+    {
+      "text-black-500": name !== activeLink,
+      "text-blue-700": name === activeLink,
+    },
+    "hover:text-blue-700 flex items-center gap-1 transition duration-150"
   );
 
   return (
-    <Link to={to}>
+    <Link to={to} onClick={() => onClick(name)}>
       <div className={linkStyles}>
         {Icon && <Icon />}
         {name}
