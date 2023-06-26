cfaa57096bbd3667cebda9b3a5959993fd07394d
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-06-26 17:05:25 +0200

    style: add margin to offers

--- src/components/CarsList.js
+++ src/components/CarsList.js
@@ -28,7 +28,7 @@ function CarsList(props) {
 
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 m-8">
         {itemsToShow.map((car) => (
           <CarsListItem
             key={car.id}
