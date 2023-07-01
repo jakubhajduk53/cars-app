eec1b198ea3e3ef27a35bb5cfe10ea456d6e387f
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-01 19:04:06 +0200

    style: remove unused class

--- src/App.js
+++ src/App.js
@@ -17,8 +17,9 @@ function App() {
     dispatch(resetCars());
     dispatch(resetPage());
   }, [href]);
+
   return (
-    <div className="App flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex flex-grow">
         <Routes>
