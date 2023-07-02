4f003824390b6882360ef940dd0e4e3f8d899ca1
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-02 16:49:38 +0200

    style: update styles in HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,10 @@
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+
 function HomePage() {
   return (
     <div
-      className="flex w-full justify-center items-center bg-cover bg-center bg-no-repeat"
+      className="flex justify-center items-center w-full bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage:
           "url(https://cdn.pixabay.com/photo/2021/12/18/15/51/car-6879134_1280.jpg)",
@@ -12,7 +13,7 @@ function HomePage() {
       <Link to="/cars-for-sale">
         <Button
           value="Get Started"
-          className="relative overflow-hidden bg-transparent hover:bg-opacity-70 text-white uppercase border border-gray"
+          className="bg-transparent hover:bg-opacity-70 text-white uppercase border border-gray"
         />
       </Link>
     </div>
