f56b031ccbeeaee8718f2b776b7e37c13fa31fc7
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-03 17:23:34 +0200

    feat: add userSlice

---
+++ src/store/slices/userSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { supabase } from "../../lib/supabaseClient";
+
+export const checkUser = createAsyncThunk("user/checkUser", async () => {
+  const { user, error } = await supabase.auth.session();
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    return user;
+  }
+});
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(checkUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(checkUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(checkUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const userReducer = userSlice.reducer;
