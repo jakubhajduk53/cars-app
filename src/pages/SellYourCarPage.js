93817e43721eb2db381f3d90bf550fa697e4eb10
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-04 17:42:56 +0200

    feat: update car selling

--- src/pages/SellYourCarPage.js
+++ src/pages/SellYourCarPage.js
@@ -2,13 +2,24 @@ import { useState } from "react";
 import { supabase } from "../lib/supabaseClient";
 import { nanoid } from "nanoid";
 import Button from "../components/Button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addCar } from "../store";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { createSelector } from "@reduxjs/toolkit";
+import { Link } from "react-router-dom";
+
+const checkIsLoggedIn = (state) => state.user.isLoggedIn;
+
+const selectIsLoggedIn = createSelector(
+  [checkIsLoggedIn],
+  (isLoggedIn) => isLoggedIn
+);
 
 function SellYourCarPage() {
   const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   const validationSchema = Yup.object().shape({
     name: Yup.string().required("Car name is required"),
@@ -55,113 +66,124 @@ function SellYourCarPage() {
 
   return (
     <div className="flex w-full justify-center py-5">
-      <div>
-        <h2 className="text-lg font-bold mb-4 text-center">Sell your car</h2>
-        <Formik
-          initialValues={{
-            name: "",
-            year_of_production: 0,
-            price: 0,
-            location: "",
-            image_url: null,
-          }}
-          onSubmit={handleSubmit}
-          validationSchema={validationSchema}
-        >
-          {({ setFieldValue, errors, touched }) => (
-            <Form className="flex flex-col px-4 py-2 w-full max-w-md">
-              <label htmlFor="name" className="mb-2 mt-4">
-                Car name:
-              </label>
-              <Field
-                type="text"
-                id="name"
-                name="name"
-                className="p-2 border border-gray-300 rounded"
-                placeholder="Car name"
-              />
-              <ErrorMessage
-                name="name"
-                component="div"
-                className="error mb-4 text-red-500"
-              />
+      {isLoggedIn ? (
+        <div>
+          <h2 className="text-lg font-bold mb-4 text-center">Sell your car</h2>
+          <Formik
+            initialValues={{
+              name: "",
+              year_of_production: 0,
+              price: 0,
+              location: "",
+              image_url: null,
+            }}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+          >
+            {({ setFieldValue, errors, touched }) => (
+              <Form className="flex flex-col px-4 py-2 w-full max-w-md">
+                <label htmlFor="name" className="mb-2 mt-4">
+                  Car name:
+                </label>
+                <Field
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="p-2 border border-gray-300 rounded"
+                  placeholder="Car name"
+                />
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className="error mb-4 text-red-500"
+                />
 
-              <label htmlFor="year" className="mb-2 mt-4">
-                Year of production:
-              </label>
-              <Field
-                type="number"
-                id="year"
-                name="year_of_production"
-                className="p-2 border border-gray-300 rounded"
-                placeholder="Year of production"
-              />
-              <ErrorMessage
-                name="year_of_production"
-                component="div"
-                className="error mb-4 text-red-500"
-              />
+                <label htmlFor="year" className="mb-2 mt-4">
+                  Year of production:
+                </label>
+                <Field
+                  type="number"
+                  id="year"
+                  name="year_of_production"
+                  className="p-2 border border-gray-300 rounded"
+                  placeholder="Year of production"
+                />
+                <ErrorMessage
+                  name="year_of_production"
+                  component="div"
+                  className="error mb-4 text-red-500"
+                />
 
-              <label htmlFor="price" className="mb-2 mt-4">
-                Price:
-              </label>
-              <Field
-                type="number"
-                id="price"
-                name="price"
-                className="p-2 border border-gray-300 rounded"
-                placeholder="Price"
-              />
-              <ErrorMessage
-                name="price"
-                component="div"
-                className="error  mb-4 text-red-500"
-              />
+                <label htmlFor="price" className="mb-2 mt-4">
+                  Price:
+                </label>
+                <Field
+                  type="number"
+                  id="price"
+                  name="price"
+                  className="p-2 border border-gray-300 rounded"
+                  placeholder="Price"
+                />
+                <ErrorMessage
+                  name="price"
+                  component="div"
+                  className="error  mb-4 text-red-500"
+                />
 
-              <label htmlFor="location" className="mb-2 mt-4">
-                Location:
-              </label>
-              <Field
-                type="text"
-                id="location"
-                name="location"
-                className="p-2 border border-gray-300 rounded"
-                placeholder="Location"
-              />
-              <ErrorMessage
-                name="location"
-                component="div"
-                className="error  mb-4 text-red-500"
-              />
+                <label htmlFor="location" className="mb-2 mt-4">
+                  Location:
+                </label>
+                <Field
+                  type="text"
+                  id="location"
+                  name="location"
+                  className="p-2 border border-gray-300 rounded"
+                  placeholder="Location"
+                />
+                <ErrorMessage
+                  name="location"
+                  component="div"
+                  className="error  mb-4 text-red-500"
+                />
 
-              <label htmlFor="image" className="mb-2 mt-4">
-                Image:
-              </label>
-              <input
-                type="file"
-                id="image"
-                accept="image/png, image/jpeg, image/jpg"
-                onChange={(event) => {
-                  setFieldValue("image_url", event.currentTarget.files[0]);
-                  setImagePreview(
-                    URL.createObjectURL(event.currentTarget.files[0])
-                  );
-                }}
-              />
-              {errors.image_url && touched.image_url && (
-                <div className="error mb-4 text-red-500">
-                  {errors.image_url}
-                </div>
-              )}
-              {imagePreview && (
-                <img src={imagePreview} alt="Preview" className="mb-4" />
-              )}
+                <label htmlFor="image" className="mb-2 mt-4">
+                  Image:
+                </label>
+                <input
+                  type="file"
+                  id="image"
+                  accept="image/png, image/jpeg, image/jpg"
+                  onChange={(event) => {
+                    setFieldValue("image_url", event.currentTarget.files[0]);
+                    setImagePreview(
+                      URL.createObjectURL(event.currentTarget.files[0])
+                    );
+                  }}
+                />
+                {errors.image_url && touched.image_url && (
+                  <div className="error mb-4 text-red-500">
+                    {errors.image_url}
+                  </div>
+                )}
+                {imagePreview && (
+                  <img src={imagePreview} alt="Preview" className="mb-4" />
+                )}
 
-              <Button type="submit" value="Confirm" className="w-full mt-4" />
-            </Form>
-          )}
-        </Formik>
-      </div>
+                <Button type="submit" value="Confirm" className="w-full mt-4" />
+              </Form>
+            )}
+          </Formik>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center">
+          <p className="mb-5 text-lg text-blue-400">
+            You must be logged in to sell your car!
+          </p>
+          <Link to="/menu">
+            <p className="hover:text-blue-500 cursor-pointer ">Log in now!</p>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
