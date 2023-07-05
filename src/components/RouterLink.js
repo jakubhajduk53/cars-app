c22ff6b494f8c143f79c9a39d1b51933494f9950
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-06 00:24:18 +0200

    feat: update classNames in RouterLink

--- src/components/RouterLink.js
+++ src/components/RouterLink.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 
-const RouterLink = ({ to, icon: Icon, name, active }) => {
+const RouterLink = ({ to, icon: Icon, name, active, className }) => {
   const linkStyles = classNames(
     "px-2 py-4 text-lg",
     {
       "text-black-500": !active,
       "text-blue-700": active,
     },
-    "hover:text-blue-700 flex items-center gap-1 transition duration-150 md:px-8"
+    "hover:text-blue-700 flex items-center gap-1 transition duration-150 md:px-8",
+    className
   );
 
   return (
