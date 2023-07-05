517337253b17b87df08df06ab179fb93d63f5a7f
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-05 20:32:56 +0200

    feat: add redirect in menu

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { createSelector } from "@reduxjs/toolkit";
+import { useNavigate } from "react-router-dom";
+
+const getUserStatus = (state) => state.user.isLoggedIn;
+
+const selectUserStatus = createSelector(
+  [getUserStatus],
+  (isLoggedIn) => isLoggedIn
+);
 
 function MenuPage() {
+  const navigate = useNavigate();
+
+  const isLoggedIn = useSelector(selectUserStatus);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/menu/panel");
+    } else {
+      navigate("/menu/login");
+    }
+  }, []);
+
   return (
     <div className="flex w-full justify-center">
       <Outlet />
