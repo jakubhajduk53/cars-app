86608bab9730d76323b851b9677d9ba3b4273e22
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-06 20:28:58 +0200

    style: update styles in CarListItem

--- src/components/CarsListItem.js
+++ src/components/CarsListItem.js
@@ -8,25 +8,29 @@ function CarsListItem(props) {
   const car = props.car;
 
   const CarsListItemStyles = classNames(
-    "flex items-center px-4 py-2 w-full h-full justify-center border border-gray-300 hover:border-black",
+    "flex items-center justify-center px-4 py-2 gap-8 xl:gap-16 w-full h-full border border-gray-300 hover:border-black",
     props.className
   );
   return (
     <div className={CarsListItemStyles}>
-      <img
-        src={car.image_url}
-        alt={car.name}
-        className="w-48 h-48 mr-8 border border-gray-300 xl:mr-32 md:w-64 md:h-64"
-      />
-      <div>
-        <p className="text-xl font-semibold mb-2 md:text-2xl">{car.name}</p>
-        <p className="text-gray-600 text-md mb-2 md:text-lg">
+      <div className="shrink-0">
+        <img
+          src={car.image_url}
+          alt={car.name}
+          className="w-32 h-32 sm:w-48 sm:h-48 md:w-64 md:h-64 border border-gray-300"
+        />
+      </div>
+      <div className="w-1/2">
+        <p className="text-xl font-semibold md:text-2xl break-words truncate mb-2">
+          {car.name}
+        </p>
+        <p className="text-gray-600 text-md md:text-lg mb-2">
           Cost: ${car.price}
         </p>
-        <p className="text-gray-600 text-md mb-2 md:text-lg">
+        <p className="text-gray-600 text-md md:text-lg mb-2">
           Year of production: {car.year_of_production}
         </p>
-        <p className="text-gray-600 text-md mb-4 md:text-lg">
+        <p className="text-gray-600 text-md md:text-lg break-words truncate mb-4">
           Location: {car.location}
         </p>
         <Button
