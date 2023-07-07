048c94725526b5b726b72dd1a6ea5f22b70883ce
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-07 17:12:18 +0200

    style: update modal styles

--- src/pages/CarsForSalePage.js
+++ src/pages/CarsForSalePage.js
@@ -47,7 +47,7 @@ function CarsForSalePage() {
       <ReactModal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        className="shadow-lg bg-white rounded-lg w-[30rem] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+        className="shadow-lg bg-white p-5 max-h-screen overflow-y-auto rounded-lg absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
         overlayClassName="fixed inset-0 bg-black bg-opacity-50"
         ariaHideApp={false}
       >
