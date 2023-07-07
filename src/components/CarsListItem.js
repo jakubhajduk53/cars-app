9ec86e0347375d3f43ad2380d8524eada8dcc3b9
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-07 02:29:05 +0200

    style: change styles in CarsListItem

--- src/components/CarsListItem.js
+++ src/components/CarsListItem.js
@@ -5,7 +5,7 @@ import { changeSelectedCar } from "../store";
 
 function CarsListItem(props) {
   const CarsListItemStyles = classNames(
-    "grid grid-cols-2 border border-gray-300 hover:border-black p-5",
+    "grid grid-cols-8 border border-gray-300 hover:border-black p-5",
     props.className
   );
 
@@ -15,29 +15,31 @@ function CarsListItem(props) {
 
   return (
     <div className={CarsListItemStyles}>
-      <div className="">
+      <div className="grid col-span-3">
         <img
           src={car.image_url}
           alt={car.name}
-          className="w-36 h-36 sm:w-52 sm:h-52 md:w-72 md:h-72 border-2 border-blue-300 rounded-xl shadow-2xl shadow-blue-200"
+          className="w-28 h-28 sm:w-52 sm:h-52 md:w-72 md:h-72 self-center border-2 border-blue-300 rounded-xl shadow-2xl shadow-blue-200"
         />
       </div>
-      <div className="grid">
-        <p className="text-xl font-semibold md:text-3xl  break-words truncate">
+      <div className="grid col-span-3">
+        <p className="text-md font-semibold sm:text-xl md:text-3xl break-words truncate">
           {car.name}
         </p>
-        <p className="text-gray-600 text-md md:text-xl italic">
+        <p className="text-gray-600 text-sm sm:text-lg md:text-xl italic">
           Cost: ${car.price}
         </p>
-        <p className="text-gray-600 text-md md:text-xl italic">
+        <p className="text-gray-600 text-sm sm:text-lg md:text-xl italic">
           Year of production: {car.year_of_production}
         </p>
-        <p className="text-gray-600 text-md md:text-xl italic break-words truncate">
+        <p className="text-gray-600 text-sm sm:text-lg md:text-xl italic break-words truncate">
           Location: {car.location}
         </p>
+      </div>
+      <div className="grid col-span-2">
         <Button
           value="Check Availability"
-          className="justify-self-end shadow-xl"
+          className="justify-self-end self-end shadow-xl px-0 py-0 text-sm md:text-base w-20 md:w-40 md:h-12 break-words"
           onClick={() => {
             dispatch(changeSelectedCar(car));
             props.openModal();
