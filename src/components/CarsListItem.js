cc782a8a33762c4e233bd3ab7d76ca630ed96a9d
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-09 22:13:14 +0200

    style: update CarsListItem styles

--- src/components/CarsListItem.js
+++ src/components/CarsListItem.js
@@ -5,7 +5,7 @@ import { selectCar, deleteYourCar } from "../store";
 
 function CarsListItem(props) {
   const CarsListItemStyles = classNames(
-    "grid grid-cols-8 border border-gray-300 hover:border-black p-5",
+    "grid grid-cols-8 border border-gray-300 hover:border-black p-5 gap-5",
     props.className
   );
 
@@ -15,12 +15,20 @@ function CarsListItem(props) {
 
   return (
     <div className={CarsListItemStyles}>
-      <div className="grid col-span-3">
-        <img
-          src={car.image_url}
-          alt={car.name}
-          className="w-28 h-28 sm:w-52 sm:h-52 md:w-72 md:h-72 self-center border-2 border-blue-300 rounded-xl shadow-2xl shadow-blue-200"
-        />
+      <div className="grid col-span-3 relative justify-content-center ">
+        <div
+          className="absolute inset-0 blur-sm bg-cover -m-1"
+          style={{
+            backgroundImage: `url(${car.image_url})`,
+          }}
+        ></div>
+        <div className="flex justify-center items-center">
+          <img
+            src={car.image_url}
+            alt={car.name}
+            className="object-scale-down h-28 sm:h-52 md:h-72 z-10 border rounded-xl"
+          />
+        </div>
       </div>
       <div className="grid col-span-3">
         <p className="text-md font-semibold sm:text-xl md:text-3xl break-words truncate">
